9051a2966b6168243d0107938a59607c6b83f64a
Paco Fuentes <pacofuentes.work@gmail.com>
2024-01-10 13:00:46 +0100

    feat(page): add image compnent

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,10 @@
-import { Montserrat } from 'next/font/google';
+import { Lusitana, Montserrat } from 'next/font/google';
 
-export const montserrat = Montserrat({subsets: ['latin'], weight: ['200', '400']})
+export const montserrat = Montserrat({
+  subsets: ['latin'],
+  weight: ['200', '400'],
+});
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
